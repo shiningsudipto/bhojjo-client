363d9ff13e75c1894341469934be5abb3236f299
Sudipta Das <shiningsudipto@gmail.com>
2024-11-16 23:41:00 +0600

    route setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <main>
+      <p>Header</p>
       <h1 className="text-2xl bg-green-500">Bhojjo</h1>
-    </>
+      <Outlet />
+      <p>Footer</p>
+    </main>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes.tsx";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <RouterProvider router={router} />
+      {/* </PersistGate> */}
     </Provider>
   </StrictMode>
 );
