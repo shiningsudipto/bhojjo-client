fb927b1f273c829867c1cbcbd409cf15d9a4e96e
Bayshakhe <bayshakhepramanik@gmail.com>
2024-11-17 22:14:51 +0600

    navbar -> navbar initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import NavbarDefault from "./components/shared/Navbar";
 
 function App() {
   return (
     <main>
-      <p>Header</p>
+      <NavbarDefault/>   
       <h1 className="text-2xl bg-green-500">Bhojjo</h1>
       <Outlet />
       <p>Footer</p>
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+
+ const NavbarDefault = () => {
+  const [openNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 960) setOpenNav(false);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="flex items-center gap-x-2 p-1 font-medium"
+      >
+        <a href="#" className="flex items-center">
+          Retail 
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="flex items-center gap-x-2 p-1 font-medium"
+      >
+        <a href="#" className="flex items-center">
+          Manufacturer 
+        </a>
+      </Typography>
+    </ul>
+  );
+
+  return (
+    <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+        <div className="flex gap-2">
+        <img style={{width: "100px"}} src="https://vojjo.io/wp-content/uploads/2021/08/cropped-logo_transparent-1024x304.png" alt="" />
+        <div className="hidden lg:block">{navList}</div>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <Button variant="text" size="sm" className="hidden lg:inline-block">
+            Hotline: <span className="text-primary">019293999</span>
+          </Button>
+          <Button
+            variant="gradient"
+            size="sm"
+            className="hidden lg:inline-block"
+          >
+          BN
+          </Button>
+        </div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <Collapse open={openNav} className="lg:hidden">
+        <div className="container mx-auto">
+          {navList}
+          <div className="flex items-center gap-x-1">
+            <Button fullWidth variant="text" size="sm" className="">
+              BN
+            </Button>
+          </div>
+        </div>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default NavbarDefault
