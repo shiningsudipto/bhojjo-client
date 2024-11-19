9625aee9e839bfc70404715154ad44496aa4a60a
Sudipta Das <shiningsudipto@gmail.com>
2024-11-20 00:47:04 +0600

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import NavMenu from "../src/components/shared/NavMenu.jsx";
 
 function App() {
   return (
     <main>
-      <p>Header</p>
-      <h1 className="text-2xl bg-green-500">Bhojjo</h1>
+      <NavMenu />
       <Outlet />
       <p>Footer</p>
     </main>
---
+++ src/components/shared/NavMenu.jsx
@@ -0,0 +1,171 @@
+import React, { useEffect, useState } from "react";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+} from "@material-tailwind/react";
+import {
+  ChevronDownIcon,
+  Bars3Icon,
+  XMarkIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+
+function NavListMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <Menu
+        open={isMenuOpen}
+        handler={setIsMenuOpen}
+        placement="bottom"
+        allowHover={true}
+      >
+        <MenuHandler>
+          <Typography as="div" variant="small" className="font-medium">
+            <ListItem
+              className="flex items-center gap-2 py-2 pr-4 font-medium text-white"
+              selected={isMenuOpen || isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen((cur) => !cur)}
+            >
+              Blocks
+              <ChevronDownIcon
+                strokeWidth={2.5}
+                className={`hidden h-3 w-3 transition-transform lg:block ${
+                  isMenuOpen ? "rotate-180" : ""
+                }`}
+              />
+              <ChevronDownIcon
+                strokeWidth={2.5}
+                className={`block h-3 w-3 transition-transform lg:hidden ${
+                  isMobileMenuOpen ? "rotate-180" : ""
+                }`}
+              />
+            </ListItem>
+          </Typography>
+        </MenuHandler>
+        <MenuList className="hidden rounded-xl lg:block">
+          <MenuItem>React</MenuItem>
+          <MenuItem>TailwindCSS</MenuItem>
+        </MenuList>
+      </Menu>
+      <div className="block lg:hidden">
+        <Collapse open={isMobileMenuOpen}>
+          <MenuItem>React</MenuItem>
+          <MenuItem>TailwindCSS</MenuItem>
+        </Collapse>
+      </div>
+    </React.Fragment>
+  );
+}
+
+function NavList() {
+  return (
+    <List className="mb-6 mt-4 p-0 lg:mb-0 lg:mt-0 lg:flex-row lg:p-1">
+      <Typography
+        as="a"
+        href="#"
+        variant="small"
+        color="white"
+        className="font-medium"
+      >
+        <ListItem className="flex items-center gap-2 py-2 pr-4">Pages</ListItem>
+      </Typography>
+      <Typography
+        as="a"
+        href="#"
+        variant="small"
+        color="white"
+        className="font-medium"
+      >
+        <ListItem className="flex items-center gap-2 py-2 pr-4">
+          Account
+        </ListItem>
+      </Typography>
+      <NavListMenu />
+      <Typography
+        as="a"
+        href="#"
+        variant="small"
+        color="white"
+        className="font-medium"
+      >
+        <ListItem className="flex items-center gap-2 py-2 pr-4">Docs</ListItem>
+      </Typography>
+    </List>
+  );
+}
+
+const NavMenu = () => {
+  const [openNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  return (
+    <>
+      <div className="px-10 py-4 flex items-center justify-between">
+        <div>
+          <img src="bhojjo.png" alt="" className="w-[120px]" />
+        </div>
+        <div className="">
+          <div className="inline-flex gap-2">
+            <div className="bg-primary rounded-full flex items-center">
+              <PhoneIcon className="size-7  text-white p-1 " strokeWidth={2} />
+            </div>
+            <p className="mt-1">
+              হটলাইন <span className="font-bold">01321215454</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Navbar className="mx-auto px-4 py-1 bg-primary text-white rounded-none">
+        <div className="flex items-center justify-between text-white">
+          <div className="hidden lg:block">
+            <NavList />
+          </div>
+          <div className="hidden gap-2 lg:flex">
+            <Button variant="outlined" color="white" size="sm">
+              Log In
+            </Button>
+          </div>
+          <IconButton
+            variant="text"
+            className="lg:hidden"
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+            ) : (
+              <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+            )}
+          </IconButton>
+        </div>
+        <Collapse open={openNav}>
+          <NavList />
+          <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden">
+            <Button variant="outlined" size="sm" fullWidth>
+              Log In
+            </Button>
+          </div>
+        </Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavMenu;
