8225f919a55d64b77698303baa26ba073e427f5d
Sudipta Das <shiningsudipto@gmail.com>
2024-11-21 00:46:39 +0600

    banner section added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@material-tailwind/react";
+
 const Home = () => {
   return (
     <div>
-      <p>Hello, Home!</p>
+      <div className="section-gap-x banner py-[100px] grid grid-cols-2">
+        <div className="space-y-8">
+          <h1 className="text-5xl font-bold leading-[72px]">
+            রিটেইল ব্যবসার পরিপূর্ণ সমাধান
+          </h1>
+          <h2 className="text-2xl font-medium">
+            বিশাল পণ্য সম্ভার | প্রতিদিন ডেলিভারি | বাকিতে পণ্য
+          </h2>
+          <Button className="text-base bg-primary hover:bg-primary-500">
+            ব্যবসা শুরু করুন
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
