72578246fa41d8e8d59516729e62edcd7a378c48
Sudipta Das <shiningsudipto@gmail.com>
2024-12-10 10:54:42 +0600

    minor changes

--- src/components/layouts/AccountLayout.tsx
+++ src/components/layouts/AccountLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import Footer from "../shared/Footer";
 import NavMenu from "../shared/NavMenu";
 
